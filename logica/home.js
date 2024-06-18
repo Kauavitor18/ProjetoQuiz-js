b23b25a3d8406bb8ecdd68caedbea6d0b77156c1
Kauavitor18 <118356349+Kauavitor18@users.noreply.github.com>
2024-06-17 23:51:34 -0300

    Implementado uma nova page e correcao de bugs

--- logica/home.js
+++ logica/home.js
@@ -4,11 +4,47 @@ function conferir(){
     if(resposta && resposta.value == "b"){
         document.getElementById("one").innerHTML = "Resposta correta";
         document.getElementById("one").style.color = "green";
+        proximaPage();
     }
     else{
+        sairTempo();
+    }
+    function proximaPage(){
+        setTimeout(function(){
+        location.href = "home2.html";
+        }, 1000);
+    }
+    function sairTempo(){
         document.getElementById("one").innerHTML = "Resposta errada";
         document.getElementById("one").style.color = "red";
-        location.href = "index.html";
+        setTimeout(function(){
+            location.href = "index.html";
+        }, 1000);
+    }
+}
+
+// HOME 2
+function conferir2(){
+    const resposta2 = document.querySelector('input[name="quiz2"]:checked');
+    if(resposta2 && resposta2.value == "a"){
+        document.getElementById("one").innerHTML = "Resposta correta";
+        document.getElementById("one").style.color = "green";
+        proximaPage2();
+    }
+    else{
+        sairTempo();
+    }
+    function proximaPage2(){
+        setTimeout(function(){
+        location.href = "home3.html";
+        }, 1000);
+    }
+    function sairTempo(){
+        document.getElementById("one").innerHTML = "Resposta errada";
+        document.getElementById("one").style.color = "red";
+        setTimeout(function(){
+            location.href = "index.html";
+        }, 1000);
     }
 }
 
--- logica/index.js
+++ logica/index.js
@@ -3,12 +3,12 @@
 const hora = new Date().getHours();
 console.log(hora)
 
-if(hora < 12){
+if(hora > 5 && hora < 12){
     document.getElementById("welcome").innerHTML = "BOM DIA " + "<br> SEJA BEM VINDO AO QUIZ GAME";
 }else if(hora > 12 && hora < 18){
     document.getElementById("welcome").innerHTML = "BOA TARDE " +"<br> SEJA BEM VINDO AO QUIZ GAME";
 }
-else{
+else if(hora > 18 && hora < 24){
     document.getElementById("welcome").innerHTML = "BOA NOITE " +"<br> SEJA BEM VINDO AO QUIZ GAME";
 }
 
